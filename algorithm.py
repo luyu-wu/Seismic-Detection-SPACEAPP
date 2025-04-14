a7d286e883c5d30aa4b53ee7878c02c85ae8efa3
luyu-wu <luyuwuvk@gmail.com>
2025-04-13 20:37:44 -0400

    clean code

--- algorithm.py
+++ algorithm.py
@@ -8,41 +8,44 @@ block_large = 50000
 block_small = 10000
 
 # for mars
-#block_large = 500
-#block_small = 100
+# block_large = 500
+# block_small = 100
 
 plt.show()
+
+
 @njit(parallel=True)
-def forwardMarch(time,vel):
+def forwardMarch(time, vel):
     factor = 6000
 
-    time = np.append(np.zeros(block_large),time)
-    vel = np.append(np.zeros(block_large),vel)
-    time = np.append(time,np.repeat(time[-1],block_large))
-    vel = np.append(vel,np.zeros(block_large))
-    num_frames = int((len(vel) - block_large)/factor)
+    time = np.append(np.zeros(block_large), time)
+    vel = np.append(np.zeros(block_large), vel)
+    time = np.append(time, np.repeat(time[-1], block_large))
+    vel = np.append(vel, np.zeros(block_large))
+    num_frames = int((len(vel) - block_large) / factor)
 
     rtm_array_small = np.zeros(num_frames)
     rtm_array_large = np.zeros(num_frames)
     time_array = np.zeros(num_frames)
-    
-    for i in prange(num_frames):
-        index = i*factor
-        # change to leading block algorithm (smaller block leading)
-        subset_small = vel[index+block_large-block_small:index+block_large]
-        subset_large = vel[index:index+block_large]
 
-        time_array[i] = time[index+block_large]        
+    for i in prange(num_frames):
+        index = i * factor
+        # change to leading block algorithm (smaller block leading)
+        subset_small = vel[index + block_large - block_small : index + block_large]
+        subset_large = vel[index : index + block_large]
+
+        time_array[i] = time[index + block_large]
         rtm_array_small[i] = np.average(np.abs(subset_small))
         rtm_array_large[i] = np.average(np.abs(subset_large))
-    return time_array,rtm_array_small,rtm_array_large
+    return time_array, rtm_array_small, rtm_array_large
 
-def showGraph(time,vel):
-    timestep = time[-1]/len(time)
 
-    fig, (filter_ax,rtm_rat_ax) = plt.subplots(2)
+def showGraph(time, vel):
+    timestep = time[-1] / len(time)
 
-    filter_ax.plot(time,vel)
+    fig, (filter_ax, rtm_rat_ax) = plt.subplots(2)
+
+    filter_ax.plot(time, vel)
     rtm_rat_ax.set_xlabel("Time (s)")
     filter_ax.set_ylabel("Velocity (m/s)")
     rtm_rat_ax.set_ylabel("Velocity (m/s)")
@@ -50,39 +53,44 @@ def showGraph(time,vel):
     plt.setp(filter_ax.get_xticklabels(), visible=False)
     filter_ax.grid()
 
-    filter_ax.set_xlim([time[0],time[-1]])
+    filter_ax.set_xlim([time[0], time[-1]])
 
-    time_rtm,rtm_ratio_small,rtm_ratio_large = forwardMarch(time,vel)
-    
-    rtm_rat_ax.plot(time_rtm,rtm_ratio_small-rtm_ratio_large,label="Difference")
-    rtm_rat_ax.fill_between(time_rtm, rtm_ratio_small-rtm_ratio_large, 0,alpha=0.3, facecolor='#089FFF')
+    time_rtm, rtm_ratio_small, rtm_ratio_large = forwardMarch(time, vel)
 
-    rtm_rat_ax.plot(time_rtm,rtm_ratio_small,label="Small Box")
-    rtm_rat_ax.fill_between(time_rtm, rtm_ratio_small, 0,alpha=0.3, facecolor='#FF8419')
+    rtm_rat_ax.plot(time_rtm, rtm_ratio_small - rtm_ratio_large, label="Difference")
+    rtm_rat_ax.fill_between(
+        time_rtm, rtm_ratio_small - rtm_ratio_large, 0, alpha=0.3, facecolor="#089FFF"
+    )
 
-    rtm_rat_ax.plot(time_rtm,rtm_ratio_large,label="Large Box")
-    rtm_rat_ax.fill_between(time_rtm, rtm_ratio_large, 0,alpha=0.3, facecolor='#65BC85')
-    
-    rtm_rat_ax.set_xlim([time_rtm[0],time_rtm[-1]])
+    rtm_rat_ax.plot(time_rtm, rtm_ratio_small, label="Small Box")
+    rtm_rat_ax.fill_between(
+        time_rtm, rtm_ratio_small, 0, alpha=0.3, facecolor="#FF8419"
+    )
+
+    rtm_rat_ax.plot(time_rtm, rtm_ratio_large, label="Large Box")
+    rtm_rat_ax.fill_between(
+        time_rtm, rtm_ratio_large, 0, alpha=0.3, facecolor="#65BC85"
+    )
+
+    rtm_rat_ax.set_xlim([time_rtm[0], time_rtm[-1]])
     rtm_rat_ax.legend()
     rtm_rat_ax.grid()
 
     plt.show()
 
-    
-def findTime(time,vel,highpass=False,showgraphs=False):
 
-    adjustment_factor = block_small/6
+def findTime(time, vel, highpass=False, showgraphs=False):
+    adjustment_factor = block_small / 6
 
-    timestep = time[-1]/len(time)
-    
+    timestep = time[-1] / len(time)
+
     if showgraphs:
-        showGraph(time,vel)        
+        showGraph(time, vel)
 
     if highpass:
-        sos = signal.butter(10, 1.5, 'hp', fs=1/timestep, output='sos')
+        sos = signal.butter(10, 1.5, "hp", fs=1 / timestep, output="sos")
         vel = signal.sosfilt(sos, vel)
 
-    time_rtm,rtm_ratio_small,rtm_large = forwardMarch(time,vel)
+    time_rtm, rtm_ratio_small, rtm_large = forwardMarch(time, vel)
     rtm_ratio = rtm_ratio_small - rtm_large
     return time_rtm[np.argmax(rtm_ratio)] - adjustment_factor
--- whiskerPlot.py
+++ whiskerPlot.py
@@ -12,13 +12,13 @@ drawExceptions = False
 highPass = False
 
 ## Catalogs
-catalog_dir = './data/lunar/training/catalogs/'
+catalog_dir = "./data/lunar/training/catalogs/"
 if mars:
-    catalog_dir = './data/mars/training/catalogs/'
+    catalog_dir = "./data/mars/training/catalogs/"
 
-catalog_file_str = catalog_dir + 'apollo12_catalog_GradeA_final.csv'
+catalog_file_str = catalog_dir + "apollo12_catalog_GradeA_final.csv"
 if mars:
-    catalog_file_str = catalog_dir + 'Mars_InSight_training_catalog_final.csv'
+    catalog_file_str = catalog_dir + "Mars_InSight_training_catalog_final.csv"
 
 catalog = pd.read_csv(catalog_file_str)
 
@@ -27,18 +27,18 @@ residuals = np.array([])
 for i in range(length):
     ## Data
     row = catalog.iloc[i]
-    arrival_time = row['time_rel(sec)']
+    arrival_time = row["time_rel(sec)"]
 
     test_filename = row.filename
-    print(int(1000*i/length)/10,"%")
-    data_directory = './data/lunar/training/data/S12_GradeA/'
+    print(int(1000 * i / length) / 10, "%")
+    data_directory = "./data/lunar/training/data/S12_GradeA/"
     if mars:
-        data_directory = './data/mars/training/data/'
+        data_directory = "./data/mars/training/data/"
 
-    csv_file = f'{data_directory}{test_filename}.csv' # Lunar
+    csv_file = f"{data_directory}{test_filename}.csv"  # Lunar
     if mars:
-        csv_file = f'{data_directory}{test_filename}' # Lunar
-    
+        csv_file = f"{data_directory}{test_filename}"  # Lunar
+
     if os.path.exists(csv_file):
         data_cat = pd.read_csv(csv_file)
 
@@ -46,37 +46,45 @@ for i in range(length):
         csv_times = 0
         csv_data = 0
         if not mars:
-            csv_times = np.array(data_cat['time_rel(sec)'].tolist())
-            csv_data = np.array(data_cat['velocity(m/s)'].tolist())
+            csv_times = np.array(data_cat["time_rel(sec)"].tolist())
+            csv_data = np.array(data_cat["velocity(m/s)"].tolist())
         else:
-            csv_times = np.array(data_cat['rel_time(sec)'].tolist())
-            csv_data = np.array(data_cat['velocity(c/s)'].tolist())
+            csv_times = np.array(data_cat["rel_time(sec)"].tolist())
+            csv_data = np.array(data_cat["velocity(c/s)"].tolist())
 
-        algo_spot = algorithm.findTime(csv_times,csv_data,highpass=highPass,showgraphs=showGraphs)
-        
-        if abs(algo_spot-arrival_time) > 1e+3 and drawExceptions: 
-            print("HIGH DIFFERENCE:",algo_spot-arrival_time,"s")
-            print("Row Number:",i)
+        algo_spot = algorithm.findTime(
+            csv_times, csv_data, highpass=highPass, showgraphs=showGraphs
+        )
+
+        if abs(algo_spot - arrival_time) > 1e3 and drawExceptions:
+            print("HIGH DIFFERENCE:", algo_spot - arrival_time, "s")
+            print("Row Number:", i)
             fig, temp_ax = plt.subplots()
 
-            temp_ax.set_title("highDelta ROW: "+str(i)+" Diff: "+str(algo_spot-arrival_time)+" s")
-            temp_ax.plot(csv_times,csv_data)
+            temp_ax.set_title(
+                "highDelta ROW: "
+                + str(i)
+                + " Diff: "
+                + str(algo_spot - arrival_time)
+                + " s"
+            )
+            temp_ax.plot(csv_times, csv_data)
             temp_ax.set_ylabel("Velocity $(m/s)$")
             temp_ax.set_xlabel("Time $(s)$")
 
-            temp_ax.axvline(x=arrival_time, c='red', label='Catalog Arrival')
-            temp_ax.axvline(x=algo_spot,c='blue',linestyle="dashed",label="Algorithm Arrival")
+            temp_ax.axvline(x=arrival_time, c="red", label="Catalog Arrival")
+            temp_ax.axvline(
+                x=algo_spot, c="blue", linestyle="dashed", label="Algorithm Arrival"
+            )
             temp_ax.legend()
-            
-            
-            
-        residuals = np.append(residuals, algo_spot-arrival_time)
+
+        residuals = np.append(residuals, algo_spot - arrival_time)
     else:
         print("FILE DNE ERROR")
 if drawExceptions:
     plt.show()
 
-plt.boxplot(residuals) 
+plt.boxplot(residuals)
 
 
 plt.show()
